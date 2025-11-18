42fdcbae21308f758e7b1aef2a746c3efe326ce8
hahaha <hahaha0417@hotmail.com>
2025-11-19 01:16:00 +0800

    fix

--- code/base/base_controller.php
+++ code/base/base_controller.php
@@ -19,15 +19,13 @@ class base_ontroller
 
     public function hahaha($view, $file_name) 
     {
-        // 2️⃣ 存成檔案
+        // 存成檔案
         $dir = dirname($file_name);
-        
-        if(!is_dir($dir))
-        {
+
+        if (!is_dir($dir)) {
             mkdir($dir, 0755, true);
         }
 
         File::put($file_name, $view);
-
     }
 }
